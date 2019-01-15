e062fa0259443216b08f523afac1f3c62f706ae0
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2019-01-15 09:02:00 -0500

    Create brewcast_free.sh

---
+++ brewcast_free.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+brew cask install \
+astro \
+avast-security \
+cheatsheet \
+docker \
+gimp \
+iterm2 \
+mysqlworkbench \
+postman \
+slack \
+virtualbox \
+visual-studio-code \
+vlc \
+zoomus
