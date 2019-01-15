2ddb573799620c3050140ddac785855cba98d631
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2019-01-15 11:27:03 -0500

    Update brew_free.sh

--- brew_free.sh
+++ brew_free.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-brew install git python python3 pipenv
+brew install bash  make git python python3 pipenv
+brew install vim --with-override-system-vi
+
+# zsh
+brew install zsh
+brew install zsh-completions
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Other useful tools
+brew install httpie
+brew install htop
