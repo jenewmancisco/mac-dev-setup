7e780ba283d3c3139b04aba0a26012b240b981ff
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2019-01-15 09:03:52 -0500

    Create brewcast_macquickplugin.sh

---
+++ brewcast_macquickplugin.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+brew cask install \
+qlcolorcode \
+qlstephen \
+qlmarkdown \
+quicklook-json \
+qlprettypatch \
+quicklook-csv \
+betterzipql \
+webpquicklook \
+suspicious-package
