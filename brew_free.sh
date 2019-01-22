d58c1bb9c4de06b01d4f47b9908a94a3c6e797d1
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2019-01-21 19:35:32 -0500

    Update brew_free.sh

--- brew_free.sh
+++ brew_free.sh
@@ -4,9 +4,9 @@ brew install bash  make git python python3 pipenv
 brew install vim --with-override-system-vi
 
 # zsh
-brew install zsh
-brew install zsh-completions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+#brew install zsh
+#brew install zsh-completions
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Other useful tools
 brew install httpie
