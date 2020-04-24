17cb6d8342bb633cba2f004ec032339f94b4b4a2
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2020-04-24 13:19:18 -0400

    Update login_exp.sh

--- sshtrust/login_exp.sh
+++ sshtrust/login_exp.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/expect
 # login_exp <password> ssh <anything>
 # login_exp <password> scp <anysrc> <anydst>
-set timeout 20
+set timeout 40
 
 set cmd [lrange $argv 1 end]
 set password [lindex $argv 0]
 
 eval spawn $cmd
-expect "assword:"
-send "$password\r";
+expect {
+    -re "assword:" {
+        send "$password\n"; exp_continue}
+        ".*Are you sure you want to continue connecting.*" {
+        send "yes\n"; exp_continue }
+    eof
+}
 interact
