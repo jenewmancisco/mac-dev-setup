8eff3ca23c7184e9298dae0b8745f624ba53d086
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2020-04-28 10:40:11 -0400

    Update brew_free.sh

--- brew_free.sh
+++ brew_free.sh
@@ -6,6 +6,14 @@ brew install vim --with-override-system-vi
 brew install git-lfs
 brew install jq
 
+#Node.js - JavaScript-based environment used to create web-servers and networked applications.
+# NPM is a “package manager” that makes installing Node “packages” fast and easy. 
+#   A package, also called a module, is just a code library that extends Node by adding useful features.
+# NPM is installed when you install Node.js
+# Test Node. To see if Node is installed, type `node -v`
+# Test NPM. To see if NPM is installed, type `npm -v`
+brew install node
+
 # zsh
 #brew install zsh
 #brew install zsh-completions
