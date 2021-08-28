6710a067e665e5dbc29665f0ead5bac0c5349f2e
jenewmancisco <46692658+jenewmancisco@users.noreply.github.com>
2021-08-28 13:50:00 -0400

    add helm3

--- brew_free.sh
+++ brew_free.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew install bash  make git python@2 python3 pipenv
+brew install bash  make git python@2 python3 pipenv helm
 brew install bash-completion@2
 brew install vim --with-override-system-vi
 brew install git-lfs
